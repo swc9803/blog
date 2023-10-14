8ebeae593fdbf84b5fe52839e33196b1159b508c
swc9803 <swc9803@gmail.com>
2023-10-14 17:54:50 +0900

    setting key env로 이동

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,11 @@
 </template>
 
 <script setup>
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+const config = useRuntimeConfig();
 
-const creds = reactive({
-  email: "",
-  password: ""
-})
-
-const nuxtApp = useNuxtApp()
-async function registerUser() {
-  try {
-    const { user } = await createUserWithEmailAndPassword(
-      nuxtApp.$auth,
-      creds.email,
-      creds.password
-    )
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      // handle error
-    }
-  }
-}
+onMounted(() => {
+  console.log(config.public.stagingUri);
+});
 </script>
 
 <style lang="scss" scoped></style>
